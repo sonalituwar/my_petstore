22826fe711a95ae275cbba7248419f835b733b99
sonalituwar <tuwar.sonali@gmail.com>
2023-08-24 11:56:35 +0530

    Initial Commit

---
+++ src/main/java/com/itvedant/my_petstore/MyPetstoreApplication.java
@@ -0,0 +1,13 @@
+package com.itvedant.my_petstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPetstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPetstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itvedant/my_petstore/MyPetstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itvedant.my_petstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPetstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
